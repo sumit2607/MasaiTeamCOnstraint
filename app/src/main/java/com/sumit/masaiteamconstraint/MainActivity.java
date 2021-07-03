e03a1b503489829f0bb1c1f9a6612c36280f2ae7
Sumit Rai <40994984+sumit2607@users.noreply.github.com>
2021-07-03 11:25:35 +0530

    Initial commit

---
+++ app/src/main/java/com/sumit/masaiteamconstraint/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sumit.masaiteamconstraint;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
